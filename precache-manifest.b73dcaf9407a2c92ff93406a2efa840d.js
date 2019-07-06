9bcb701814ae84b097a3342f63237a431c375388
ginnnnnn <aginlo@hotmail.com>
2019-07-06 16:42:40 +0800

    Updates

---
+++ precache-manifest.b73dcaf9407a2c92ff93406a2efa840d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a978f1aa50b095cc6a1a8bffa0541a17",
+    "url": "/jingjingthinking-studio/index.html"
+  },
+  {
+    "revision": "763baa9cd82fa0c5ca37",
+    "url": "/jingjingthinking-studio/static/css/3.e0f20f29.chunk.css"
+  },
+  {
+    "revision": "5deb2b681dad20c38e0b",
+    "url": "/jingjingthinking-studio/static/css/4.0335e9fe.chunk.css"
+  },
+  {
+    "revision": "3f3de1c4f7f46ddd6911",
+    "url": "/jingjingthinking-studio/static/css/main.f57783c8.chunk.css"
+  },
+  {
+    "revision": "22888eec6b34d1eb520e",
+    "url": "/jingjingthinking-studio/static/js/2.8e8d1203.chunk.js"
+  },
+  {
+    "revision": "763baa9cd82fa0c5ca37",
+    "url": "/jingjingthinking-studio/static/js/3.9bc0652a.chunk.js"
+  },
+  {
+    "revision": "5deb2b681dad20c38e0b",
+    "url": "/jingjingthinking-studio/static/js/4.c98395be.chunk.js"
+  },
+  {
+    "revision": "3f3de1c4f7f46ddd6911",
+    "url": "/jingjingthinking-studio/static/js/main.a7434fd5.chunk.js"
+  },
+  {
+    "revision": "7c13c7390ab7cc9eec99",
+    "url": "/jingjingthinking-studio/static/js/runtime~main.f3e6987b.js"
+  },
+  {
+    "revision": "404ff6b8a3809ee54ec1126fc797b4d8",
+    "url": "/jingjingthinking-studio/static/media/contact-bg-l.404ff6b8.png"
+  },
+  {
+    "revision": "3db2f6da1966c039e113d310d4670ffa",
+    "url": "/jingjingthinking-studio/static/media/contact-bg.3db2f6da.jpg"
+  },
+  {
+    "revision": "6fd45b53782cebe02aea75d5d486d769",
+    "url": "/jingjingthinking-studio/static/media/footer.6fd45b53.jpg"
+  },
+  {
+    "revision": "13389f0221f40a0ec4007386d85429b6",
+    "url": "/jingjingthinking-studio/static/media/index-bg.13389f02.png"
+  },
+  {
+    "revision": "326bd4f837a0db01d6be943ee04bd140",
+    "url": "/jingjingthinking-studio/static/media/index-gb-l.326bd4f8.jpg"
+  },
+  {
+    "revision": "793f9455e49bcfb04be9c2c48571a2d5",
+    "url": "/jingjingthinking-studio/static/media/me.793f9455.jpg"
+  }
+]);
