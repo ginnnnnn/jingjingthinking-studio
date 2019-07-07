db21b9aa7f78027450751efe002f9a67f6d8a60c
ginnnnnn <aginlo@hotmail.com>
2019-07-07 21:07:38 +0800

    fix content

--- src/components/AboutPage/aboutContent.js
+++ src/components/AboutPage/aboutContent.js
@@ -8,7 +8,7 @@ export const aboutContent = {
     And now I am a Web Developer. My life is always full of chellenges and I really really enjoy it. `,
     para_2: `As a Web Developer, I am more familiar with javascript ,React for frontend and Node as backend service. But not limit to that ,I am still a big fan using google firebase/firsestore 
     still learning any new feature and web tech. And I have a bunch of interesting courses on my waiting list on Udemy and Youtube. I am a self motivated person.
-    So let's see what can I help.`,
+    So let's see what I can help.`,
     para_3: `YOU NEED A WEBSITE! 
     Do I need a website? a lot of people asked. Yes. You definitely need. A modern Website is not only
     help you to display you products but also can schedule your timeshift. And not just a big company need a website. I would say 
@@ -19,8 +19,8 @@ export const aboutContent = {
     para_4: `Let's face some fact here. I was a head chef in several restaurants. When home delivery-service rises like Uber-eat. I witness 
     lots of restaurants closed in a few months . Why? let's do some math. Uber-eat charge you 30 to 35% and your food cost about 25% ,tax
      takes 10% and if you are a franchise, 10% more. 55% gone before you sell that meal and that's not included you labor ,rent and bills.
-     And the worst part is that as a customer you are always just a one shop in a huge online food court. 
-     I saw a few shop they start doing they owned delivery. They pay a fixed cost to a driver and charge customers delivery fee. You 
+     And the worst part is that on customer side you are always just one shop in a huge online food court. 
+     I saw a few shop they start doing their owned delivery service. They pay a fixed cost to a driver and charge customers delivery fee. You 
      get a win-win. But before that you need a website to provide online ordering and steadily promote you business.`,
     para_5: `Well,that's a lot. But that's my observation. So if you are considering to start your business or you
     already have one but in stuggle. I can help you. If you have a project need someone to join your team. I am available.
